3457c714bfe37733f05211ef68affb29a18fd8b0
yeongjaeseo <yjseo@lbstech.net>
2025-01-20 17:36:59 +0900

    feat: Add API for bldg and prepare for merge into main

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,21 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { BldgModule } from './bldg/bldg.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: '34.64.248.193',
+      host: '아이피주소',
       port: 5432,
       username: 'codelab',
-      password: 'codelab1234',
+      password: '비밀번호',
       database: 'codelab-nest',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
-    BldgModule,
   ],
   controllers: [AppController],
   providers: [AppService],
